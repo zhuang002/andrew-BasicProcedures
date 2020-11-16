7a3f671cce4de21323db9656940adc8294de6644
andre <andre@192.168.0.16>
2020-11-16 16:44:37 -0500

    changed by andrew

--- BasicProcedures/src/BasicProcedures.java
+++ BasicProcedures/src/BasicProcedures.java
@@ -1,48 +1,69 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class BasicProcedures {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] ar=new int[10];
-		Random random=new Random();
-		for (int i=0;i<10;i++) {
-			ar[i]=random.nextInt();
-			System.out.print(ar[i]+" ");
+		int[] ar = new int[10];
+		Random random = new Random();
+		for (int i = 0; i < 10; i++) {
+			ar[i] = random.nextInt(10);
+			System.out.print(ar[i] + " ");
 		}
 		System.out.println();
-		
-		int sum=getSum(ar);
-		System.out.println("sum is "+sum);
-		int max=getMax(ar);
-		System.out.println("max is "+max);
-		int min=getMin(ar);
-		System.out.println("min is "+min);
+
+		int sum = getSum(ar);
+		System.out.println("sum is " + sum);
+		int max = getMax(ar);
+		System.out.println("max is " + max);
+		int min = getMin(ar);
+		System.out.println("min is " + min);
 		sort(ar);
-		System.out.println(ar);
+		for(int i=0;i<ar.length;i++)
+			System.out.print(ar[i]+ " ");
 		
+
 	}
 
 	private static void sort(int[] ar) {
-		// TODO Auto-generated method stub
-		
+		Arrays.sort(ar);
 	}
 
 	private static int getMin(int[] ar) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		int temp = Integer.MAX_VALUE;
+
+		for (int i = 0; i < ar.length; i++) {
+			if (temp > ar[i]) {
+				temp = ar[i];
+
+			}
+
+		}
+
+		return temp;
+
 	}
 
 	private static int getMax(int[] ar) {
-		// TODO Auto-generated method stub
-		return 0;
+		int temp = Integer.MIN_VALUE;
+
+		for (int i = 0; i < ar.length; i++) {
+			if (temp < ar[i]) {
+				temp = ar[i];
+			}
+
+		}
+		return temp;
 	}
 
 	private static int getSum(int[] ar) {
-		// TODO Auto-generated method stub
-		return 0;
+		int total = 0;
+		for(int i=0;i<ar.length;i++) {
+			 total = total + ar[i];
+		}
+		return total;
 	}
-	
-	
 
 }
